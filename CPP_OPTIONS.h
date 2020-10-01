98f8384c75ed93021d8a382bbbd195925edf62c6
yfeng-github <72193232+yfeng-github@users.noreply.github.com>
2020-10-01 16:59:59 +0800

    Add files via upload

---
+++ CPP_OPTIONS.h
@@ -0,0 +1,259 @@
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!-- ViewVC :: http://www.viewvc.org/ -->
+<head>
+<title>[MITgcm] Log of /MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h</title>
+<meta name="generator" content="ViewVC 1.1.22" />
+<link rel="shortcut icon" href="/viewvc/*docroot*/images/favicon.ico" />
+<link rel="stylesheet" href="/viewvc/*docroot*/styles.css" type="text/css" />
+
+</head>
+<body>
+<div class="vc_navheader">
+<table><tr>
+<td><strong><a href="/viewvc"><span class="pathdiv">/</span></a><a href="/viewvc/MITgcm/">[MITgcm]</a><span class="pathdiv">/</span><a href="/viewvc/MITgcm/MITgcm_contrib/">MITgcm_contrib</a><span class="pathdiv">/</span><a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/">high_res_cube</a><span class="pathdiv">/</span><a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/">cube92</a><span class="pathdiv">/</span><a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/">code</a><span class="pathdiv">/</span>CPP_OPTIONS.h</strong></td>
+<td style="text-align: right;"></td>
+</tr></table>
+</div>
+<div style="float: right; padding: 5px;"><a href="http://www.viewvc.org/" title="ViewVC Home"><img src="/viewvc/*docroot*/images/viewvc-logo.png" alt="ViewVC logotype" width="240" height="70" /></a></div>
+<h1>Log of /MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h</h1>
+
+<p style="margin:0;">
+
+<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/"><img src="/viewvc/*docroot*/images/back_small.png" class="vc_icon" alt="Parent Directory" /> Parent Directory</a>
+
+| <a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=log"><img src="/viewvc/*docroot*/images/log.png" class="vc_icon" alt="Revision Log" /> Revision Log</a>
+
+
+| <a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=graph"><img src="/viewvc/*docroot*/images/cvsgraph_16x16.png" class="vc_icon" alt="View Revision Graph" /> Revision Graph</a>
+
+
+
+</p>
+
+<hr />
+<table class="auto">
+
+
+
+<tr>
+<td>Links to HEAD:</td>
+<td>
+(<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=markup">view</a>)
+(<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=co">download</a>)
+
+(<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=annotate">annotate</a>)
+</td>
+</tr>
+
+
+
+<tr>
+<td>Sticky Tag:</td>
+<td><form method="get" action="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h" style="display: inline">
+<div style="display: inline">
+<input type="hidden" name="view" value="log"/>
+
+
+<select name="pathrev" onchange="submit()">
+<option value=""></option>
+
+<optgroup label="Branches">
+
+
+<option>MAIN</option>
+
+
+</optgroup>
+
+<optgroup label="Non-branch tags">
+
+
+<option>HEAD</option>
+
+
+</optgroup>
+
+</select>
+
+<input type="submit" value="Set" />
+</div>
+</form>
+
+</td>
+</tr>
+</table>
+ 
+
+
+
+
+
+
+
+<div>
+<hr />
+
+<a name="rev1.1"></a>
+<a name="HEAD"></a>
+
+
+Revision <strong>1.1</strong> -
+
+
+(<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?revision=1.1&amp;view=markup">view</a>)
+
+
+(<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?revision=1.1&amp;view=co">download</a>)
+
+(<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?annotate=1.1">annotate</a>)
+
+
+
+- <a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=log&amp;r1=1.1">[select for diffs]</a>
+
+
+
+
+<br />
+
+<em>Thu Dec  6 02:01:50 2012 UTC</em>
+(6 years, 7 months ago)
+by <em>dimitri</em>
+
+
+<br />Branch:
+
+<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=log&amp;pathrev=MAIN"><strong>MAIN</strong></a>
+
+
+
+<br />CVS Tags:
+
+<a href="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h?view=log&amp;pathrev=HEAD"><strong>HEAD</strong></a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<pre class="vc_log">moved README.cube92 to cube92/README.cs510
+and checked in code and input files
+
+</pre>
+</div>
+
+ 
+
+
+ <hr />
+<p><a name="diff"></a>
+This form allows you to request diffs between any two revisions of this file.
+For each of the two "sides" of the diff,
+
+select a symbolic revision name using the selection box, or choose
+'Use Text Field' and enter a numeric revision.
+
+</p>
+<form method="get" action="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h" id="diff_select">
+<table cellpadding="2" cellspacing="0" class="auto">
+<tr>
+<td>&nbsp;</td>
+<td>
+<input type="hidden" name="view" value="diff"/>
+Diffs between
+
+<select name="r1">
+<option value="text" selected="selected">Use Text Field</option>
+
+<option value="1.1:MAIN">MAIN</option>
+
+<option value="1.1:HEAD">HEAD</option>
+
+</select>
+<input type="text" size="12" name="tr1"
+value="1.1"
+onchange="document.getElementById('diff_select').r1.selectedIndex=0" />
+
+and
+
+<select name="r2">
+<option value="text" selected="selected">Use Text Field</option>
+
+<option value="1.1:MAIN">MAIN</option>
+
+<option value="1.1:HEAD">HEAD</option>
+
+</select>
+<input type="text" size="12" name="tr2"
+value="1.1"
+onchange="document.getElementById('diff_select').r2.selectedIndex=0" />
+
+</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>
+Type of Diff should be a
+<select name="diff_format" onchange="submit()">
+<option value="h" selected="selected">Colored Diff</option>
+<option value="l" >Long Colored Diff</option>
+<option value="f" >Full Colored Diff</option>
+<option value="u" >Unidiff</option>
+<option value="c" >Context Diff</option>
+<option value="s" >Side by Side</option>
+</select>
+<input type="submit" value=" Get Diffs " />
+</td>
+</tr>
+</table>
+</form>
+
+
+
+<form method="get" action="/viewvc/MITgcm/MITgcm_contrib/high_res_cube/cube92/code/CPP_OPTIONS.h">
+<div>
+<hr />
+<a name="logsort"></a>
+<input type="hidden" name="view" value="log"/>
+Sort log by:
+<select name="logsort" onchange="submit()">
+<option value="cvs" >Not sorted</option>
+<option value="date" selected="selected">Commit date</option>
+<option value="rev" >Revision</option>
+</select>
+<input type="submit" value=" Sort " />
+</div>
+</form>
+
+
+<hr />
+<table>
+<tr>
+<td>&nbsp;</td>
+<td style="text-align: right;"><strong><a href="/viewvc/*docroot*/help_log.html">ViewVC Help</a></strong></td>
+</tr>
+<tr>
+<td>Powered by <a href="http://viewvc.tigris.org/">ViewVC 1.1.22</a></td>
+<td style="text-align: right;">&nbsp;</td>
+</tr>
+</table>
+</body>
+</html>
+
+
